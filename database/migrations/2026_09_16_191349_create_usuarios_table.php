5b5818c90eafc6ff325447e6f9c6359a3b5072e2
Valery3107 <vjcastro@ucundinamarca.edu.co>
2026-09-16 14:29:21 -0500

    Feature migration users

--- database/migrations/2026_09_16_191349_create_usuarios_table.php
+++ database/migrations/2026_09_16_191349_create_usuarios_table.php
@@ -12,9 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+            $table->id('id_usuario');
+            $table->string('nombre_completo');
+            $table->string('correo')->unique();
+            $table->string('contrasena');
+            $table->string('numero_documento')->unique();
+            $table->string('telefono')->nullable();
+            $table->enum('rol', ['veterinario', 'secretaria', 'administrador']);
+            $table->enum('estado', ['activo', 'inactivo'])->default('activo');
+            $table->string('foto_perfil')->nullable();
+            $table->timestamp('ultimo_acceso')->nullable();
+            $table->string('token_recuperacion')->nullable();
+            $table->timestamp('token_expiracion')->nullable();
+            $table->timestamp('fecha_registro')->useCurrent();
+});
     }
 
     /**

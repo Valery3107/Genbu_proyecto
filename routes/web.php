615e60395e5818370c12094a6ba3c80f2cd9ea32
sofiblanquicet14-collab <sofi.blanquicet14@gmail.com>
2026-09-17 18:36:32 -0500

    modificacion en ruta

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\RegistroController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/login', [LoginController::class, 'mostrarFormulario'])->name('login');
